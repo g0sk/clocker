4d8827ddc21dac6226a503f14ef000b65c69eec4
Óscar Santana <g0sk.dev@proton.me>
2022-11-15 19:31:31 +0000

    added tailwindcss

---
+++ tailwind.config.cjs
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ['./src/**/*.{ts,tsx}', './screens/**/*.{ts,tsx}', './src/components/**/*.{ts,tsx}'],
+	darkMode: 'class',
+	theme: {
+		extend: {},
+	},
+	plugins: [],
+};
